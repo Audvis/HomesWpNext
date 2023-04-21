564cebc3beb97001d4168fac7558d1f21eb24c08
Audvis <artistaudiovisual@gmail.com>
2023-04-20 23:22:47 -0600

    new commit

--- .next/static/webpack/webpack.c7fcc7ee1fa25fee.hot-update.js
+++ .next/static/webpack/webpack.c7fcc7ee1fa25fee.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ab1c12d28da10ca9"; }
+/******/ 	__webpack_require__.h = function() { return "195424efcd347a16"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
